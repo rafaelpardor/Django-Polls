7aafa828a78d9e4c550688f9b1b65db3e5d2ced2
rafaelpardor <rafaelpardor@protonmail.com>
2019-10-30 16:35:59 -0500

    Using urls

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,21 +1,17 @@
-"""mysite URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/dev/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    # the path() function is passed four arguments.
+    # Two req. route and view, and two optionals, name and kwargs
+    # Route: is a string type, and contains the URL pattern
+    # View: While the URLpattern is matching, an view function is called with an HTTPRequest
+    # Kwargs: 
+    #  Name: Naming the URL let us to refer in other parts of our django app. Leaving apart unambiguously.
     path('admin/', admin.site.urls),
+
+    # The include() function allows referencing other URLconfs.
+    # The idea of this func. is to make it easy ti plug-and-play URLs.
+    # We should use include when we include other url patterns.
+    path('polls/', include('polls.urls')),
 ]
