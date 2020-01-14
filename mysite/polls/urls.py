394d8bc90556a59244b2221706fee56390fd38d5
rafaelpardor <rafaelpardor@protonmail.com>
2020-01-13 23:11:55 -0500

    Writing new views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,5 +4,12 @@ from . import views
 
 
 urlpatterns = [
+    #/polls/
     path('', views.index, name='index'),
+    #/polls/n/
+    path('<int:question_id>/', views.detail, name='detail'),
+    #polls/n/results
+    path('<int:question_id>/results/', views.results, name='results'),
+    #polls/n/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,25 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("Hello, from polls app.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list, }
+    # request, template_name, context=None, content_type=None, status=None, using=None
+    return render(request, 'polls/index.html', context)
 
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at question %s" % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the result of question %s"
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s" % question_id)
