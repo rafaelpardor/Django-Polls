419b883ebd1d706095b78fb377f0002c24ec5639
rafaelpardor <rafaelpardor@protonmail.com>
2020-01-13 23:50:32 -0500

    Raising 404 error

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,12 +4,12 @@ from . import views
 
 
 urlpatterns = [
-    #/polls/
+    # /polls/
     path('', views.index, name='index'),
-    #/polls/n/
+    # /polls/n/
     path('<int:question_id>/', views.detail, name='detail'),
-    #polls/n/results
+    # polls/n/results
     path('<int:question_id>/results/', views.results, name='results'),
-    #polls/n/vote/
+    # polls/n/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
